1761140e813aea5086db5f0e1223012a14815cf4
Mehroze-Ali <mehrosealisohail.wah@gmail.com>
2025-08-24 02:37:14 -0700

    Add files via upload

---
+++ Week-3/week3_abstraction/Circle.java
@@ -0,0 +1,14 @@
+package week3_abstraction;
+
+class Circle extends Shape {
+    double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    double area() {
+        return Math.PI * radius * radius;
+    }
+}
